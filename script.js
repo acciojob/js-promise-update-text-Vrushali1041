775283aca6889c68aa078058d484ca4844b12fe1
Vrushali Patil <113831357+Vrushali1041@users.noreply.github.com>
2023-08-05 15:13:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+ function waitAndResolve() {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve("Hello, world!");
+                }, 1000);
+            });
+        }
+
+        function updateText() {
+            const outputElement = document.getElementById("output");
+            outputElement.textContent = "Loading...";
+
+            waitAndResolve()
+                .then((result) => {
+                    outputElement.textContent = result;
+                })
+                .catch((error) => {
+                    outputElement.textContent = "Error occurred.";
+                    console.error(error);
+                });
+        }
