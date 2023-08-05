647f97f887f1863374b96dd0c1ee2eda831cc53f
Vrushali Patil <113831357+Vrushali1041@users.noreply.github.com>
2023-08-05 15:21:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
 //your JS code here. If required.
- function waitAndResolve() {
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    resolve("Hello, world!");
-                }, 1000);
-            });
-        }
+ // function waitAndResolve() {
+ //            return new Promise((resolve, reject) => {
+ //                setTimeout(() => {
+ //                    resolve("Hello, world!");
+ //                }, 1000);
+ //            });
+ //        }
 
-        function updateText() {
-            const outputElement = document.getElementById("output");
-            outputElement.textContent = "Loading...";
+ //        function updateText() {
+ //            const outputElement = document.getElementById("output");
+ //            outputElement.textContent = "Loading...";
 
-            waitAndResolve()
-                .then((result) => {
-                    outputElement.textContent = result;
-                })
-                .catch((error) => {
-                    outputElement.textContent = "Error occurred.";
-                    console.error(error);
-                });
-        }
+        //     waitAndResolve()
+        //         .then((result) => {
+        //             outputElement.textContent = result;
+        //         })
+        //         .catch((error) => {
+        //             outputElement.textContent = "Error occurred.";
+        //             console.error(error);
+        //         });
+        // 
+
+
+// script.js
+
+function waitAndResolve() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+function updateText() {
+  const outputElement = document.getElementById("output");
+  outputElement.textContent = "Loading...";
+
+  waitAndResolve()
+    .then((result) => {
+      outputElement.textContent = result;
+    })
+    .catch((error) => {
+      outputElement.textContent = "Error occurred.";
+      console.error(error);
+    });
+}
+
+updateText(); // Call the function to update the text on page load
+
